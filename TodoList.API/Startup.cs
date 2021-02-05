7aabab22a8a7529a62f1cd773ecd6a27104c7b71
Tudor Anghelina <tudor.anghelina91@gmail.com>
2021-02-05 15:54:42 +0200

    Wrong branch, rolled back#

--- TodoList.API/Startup.cs
+++ TodoList.API/Startup.cs
@@ -28,7 +28,7 @@ namespace TodoList.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoListContext>(options => options.UseSqlite(Configuration.GetConnectionString("prod")));
+            services.AddDbContext<TodoListContext>(options => options.UseSqlite(Configuration.GetConnectionString("dev")));
             services.AddControllers();
             services.AddCors(options =>
             {
