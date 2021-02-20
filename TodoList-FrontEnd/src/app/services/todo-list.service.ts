43c8d502f1beef73079c7fb354eaf91532ef0149
Tudor Anghelina <tudor.anghelina91@gmail.com>
2021-02-20 17:34:06 +0200

    Set connection string back to dev

--- TodoList-FrontEnd/src/app/services/todo-list.service.ts
+++ TodoList-FrontEnd/src/app/services/todo-list.service.ts
@@ -11,7 +11,7 @@ export class TodoListService {
 
   constructor(private httpClient : HttpClient) { }
 
-  baseUri = "https://apidostuff.azurewebsites.net/api";
+  baseUri = "https://localhost:44307/api";
   listUri = "todolists";
   itemsUri = "todolistitems"
 
