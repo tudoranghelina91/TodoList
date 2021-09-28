9f51d491f69d455396e194178ceb2e9b5ab927dd
Tudor Anghelina <tudor.anghelina91@gmail.com>
2021-09-29 00:44:20 +0300

    Created Authentication Service

---
+++ DoStuff.API/AuthService.cs
@@ -0,0 +1,11 @@
+﻿using DoStuff.Models;
+using System;
+
+namespace DoStuff.API
+{
+    public class AuthService : IAuthService
+    {
+        public bool IsAuthorized(User user) => 
+            user != null && user.ExpiresIn > new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+    }
+}
