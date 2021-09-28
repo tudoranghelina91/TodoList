d44d96f4bd8eaf05537c6e4af3350df2cd6e37d3
Tudor Anghelina <tudor.anghelina91@gmail.com>
2021-09-29 00:53:20 +0300

    Added route activation by login status

--- TodoList-FrontEnd/src/app/app-routing.module.ts
+++ TodoList-FrontEnd/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginActivate } from './login-activate/login-activate.component';
 import { LoginComponent } from './login/login.component';
 import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -13,41 +14,60 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    component: LoginComponent,
+    component: LoginComponent
   },
   {
     path: 'lists',
     component: TodoListsComponent,
+    canActivate: [
+      LoginActivate
+    ]
   },
   {
     path: 'lists/add',
     component: TodoListComponent,
+    canActivate: [
+      LoginActivate
+    ]
   },
   {
     path: 'lists/:todoListId',
     component: TodoListComponent,
     pathMatch: 'full',
+    canActivate: [
+      LoginActivate
+    ]
   },
   {
     path: 'lists/:todoListId/add',
     component: TodoListItemComponent,
     pathMatch: 'full',
+    canActivate: [
+      LoginActivate
+    ]
   },
   {
     path: 'lists/:todoListId/:todoListItemId',
     component: TodoListItemComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [
+      LoginActivate
+    ]
   },
   {
     path: '**',
     redirectTo: 'lists',
-    pathMatch: 'full'
+    pathMatch: 'full',
+    canActivate: [
+      LoginActivate
+    ]
   }
 ];
 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [LoginActivate]
 })
 export class AppRoutingModule { }
