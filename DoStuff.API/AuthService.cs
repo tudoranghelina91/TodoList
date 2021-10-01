64209739bf15cb0a7bd8df6e0b6d41ccc8a05f11
Tudor Anghelina <tudor.anghelina91@gmail.com>
2021-10-01 10:01:30 +0300

    Removed trailing whitespace

--- DoStuff.API/AuthService.cs
+++ DoStuff.API/AuthService.cs
@@ -5,7 +5,7 @@ namespace DoStuff.API
 {
     public class AuthService : IAuthService
     {
-        public bool IsAuthorized(User user) => 
+        public bool IsAuthorized(User user) =>
             user != null && user.ExpiresIn > new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
     }
 }
