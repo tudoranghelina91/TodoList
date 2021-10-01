72b6611b8343166c3bbb20eec86ece62d2257d5a
Tudor Anghelina <tudor.anghelina91@gmail.com>
2021-10-01 10:00:14 +0300

    Removed unused fields

--- DoStuff.Models/User.cs
+++ DoStuff.Models/User.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DoStuff.Models
 {
     public class User
     {
         public long Id { get; set; }
-        public string Username { get; set; }
         public string Email { get; set; }
         public string HashedPassword { get; set; }
         public byte[] Salt { get; set; }
--- TodoList-FrontEnd/src/app/interfaces/IUser.ts
+++ TodoList-FrontEnd/src/app/interfaces/IUser.ts
@@ -1,21 +1,15 @@
 export interface User
 {
     id : number;
-    username : string;
     email : string;
     hashedPassword : string;
-    salt : string;
     accessToken : string;
-    expiresIn : number;
 }
 
 export class User
 {
     id : number;
-    username : string;
     email : string;
     hashedPassword : string;
-    salt : string;
     accessToken : string;
-    expiresIn : number;
 }
