b2bd2f473f443e37561ceace8b3d1be535b49103
Tudor Anghelina <tudor.anghelina91@gmail.com>
2022-01-16 11:46:27 +0200

    Create TokenUtil

---
+++ DoStuff.API/TokenUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+namespace DoStuff.API
+{
+    public static class TokenUtil
+    {
+        public static int GetUserId(string t)
+        {
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = Convert.ToString(t).Replace("Bearer", string.Empty).TrimStart();
+            return Convert.ToInt32(jwtSecurityTokenHandler.ReadJwtToken(token).Subject);
+        }
+    }
+}
