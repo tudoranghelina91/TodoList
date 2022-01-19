370c259a2696c466e1514635381d9f6e7f5dcabf
Tudor Anghelina <tudor.anghelina91@gmail.com>
2022-01-19 23:24:16 +0200

    Create Facebook Authentication Controller

---
+++ DoStuff.API/Controllers/FacebookAuthController.cs
@@ -0,0 +1,20 @@
+﻿using DoStuff.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DoStuff.API.Controllers
+{
+    public class FacebookAuthController : Controller
+    {
+        private readonly IFacebookAuthService _facebookAuthService;
+        public FacebookAuthController(IFacebookAuthService facebookAuthService)
+        {
+            _facebookAuthService = facebookAuthService;
+        }
+
+        public async Task <ActionResult<string>> GetAccessToken(string code)
+        {
+            return await this._facebookAuthService.GetAccessToken(code);
+        }
+    }
+}
