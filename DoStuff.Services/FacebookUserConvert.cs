196e54b296599cc1e603e514bd4efc23dc153dbf
Tudor Anghelina <tudor.anghelina91@gmail.com>
2022-01-20 00:14:30 +0200

    Add facebook user conversion

---
+++ DoStuff.Services/FacebookUserConvert.cs
@@ -0,0 +1,18 @@
+﻿using DoStuff.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoStuff.Services
+{
+    public static class Convert
+    {
+        public static User ToUser(FacebookUserData facebookUserData)
+        {
+            return new User
+            {
+                Email = facebookUserData.Email
+            };
+        }
+    }
+}
