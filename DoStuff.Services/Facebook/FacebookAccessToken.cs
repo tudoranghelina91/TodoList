088d933afe3f8c29a46f8bf75f032e2cb1317ded
Tudor Anghelina <tudor.anghelina91@gmail.com>
2022-01-21 00:42:04 +0200

    Create Facebook Access Token

---
+++ DoStuff.Services/Facebook/FacebookAccessToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+namespace DoStuff.Services.Facebook
+{
+    public class FacebookAccessToken
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+    }
+}
