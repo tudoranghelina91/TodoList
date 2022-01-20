b0653509219aa284a3a9a10219c8dac33a0a7d64
Tudor Anghelina <tudor.anghelina91@gmail.com>
2022-01-20 09:27:31 +0200

    Add user service to Facebook Auth Controller

--- DoStuff.API/Controllers/FacebookAuthController.cs
+++ DoStuff.API/Controllers/FacebookAuthController.cs
@@ -1,4 +1,6 @@
-﻿using DoStuff.Services.Facebook;
+﻿using DoStuff.Models;
+using DoStuff.Services.Facebook;
+using DoStuff.Services.Users;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -7,9 +9,11 @@ namespace DoStuff.API.Controllers
     public class FacebookAuthController : Controller
     {
         private readonly IFacebookAuthService _facebookAuthService;
-        public FacebookAuthController(IFacebookAuthService facebookAuthService)
+        private readonly IUserService _userService;
+        public FacebookAuthController(IFacebookAuthService facebookAuthService, IUserService userService)
         {
             _facebookAuthService = facebookAuthService;
+            _userService = userService;
         }
 
         public async Task <ActionResult<string>> GetAccessToken(string code)
@@ -19,7 +23,25 @@ namespace DoStuff.API.Controllers
 
         public async Task<ActionResult<FacebookUserData>> GetFacebookUserInfo(string accessToken)
         {
-            return await this._facebookAuthService.GetUserInfo(accessToken);
+            return await this._facebookAuthService.GetFacebookUserData(accessToken);
+        }
+
+        public async Task<ActionResult<string>> Login(string accessToken)
+        {
+            FacebookUserData data = await _facebookAuthService.GetFacebookUserData(accessToken);
+
+            if (data == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _userService.GetUserByEmail(data.Email);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return TokenUtil.GenerateToken(user);
         }
     }
 }
