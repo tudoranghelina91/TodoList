28a959223f5d5d5239ceaf7a6910c2efef75b824
Tudor Anghelina <tudor.anghelina91@gmail.com>
2022-01-31 09:33:08 +0200

    Redirect unauthorized calls

--- TodoList-FrontEnd/src/app/interceptors/login.interceptor.ts
+++ TodoList-FrontEnd/src/app/interceptors/login.interceptor.ts
@@ -3,14 +3,27 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class LoginInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private router : Router) {}
+
+  handleError(error : HttpErrorResponse) {
+    if (error.status != 401) {
+      return;
+    }
+    
+    localStorage.clear();
+    this.router.navigateByUrl('login');
+    return throwError(error);
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     request = request.clone({
@@ -19,6 +32,7 @@ export class LoginInterceptor implements HttpInterceptor {
       },
     });
 
-    return next.handle(request);
+    return next.handle(request)
+    .pipe(catchError(err => this.handleError(err)));
   }
 }
